c4c83b9a7aa0af1bae630023084b4f2d8c2168a5
Nirodya Pussadeniya <en95142@sjp.ac.lk>
2022-06-13 23:11:09 +0530

    updated

--- school_van_app/lib/screens/parents/parent_profile_main.dart
+++ school_van_app/lib/screens/parents/parent_profile_main.dart
@@ -438,8 +438,8 @@ class _ParentProfileMainState extends State<ParentProfileMain> {
                     } else {
                       return Center(
                           child: Text(
-                        "No Registered children",
-                        style: TextStyle(fontSize: 25, color: Colors.blue[900]),
+                        "Please add the information of your child!",
+                        style: TextStyle(fontSize: 16, color: Colors.blue[900]),
                       ));
                     }
                   }),
